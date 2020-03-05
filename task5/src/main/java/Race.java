a27ef79125e715ccd472cb55fc7185b49eed30d6
DmitriyNovikov0 <47492056+DmitriyNovikov0@users.noreply.github.com>
2020-03-05 22:50:53 +0600

    дз 5

--- task5/src/main/java/Race.java
+++ task5/src/main/java/Race.java
@@ -1,10 +1,17 @@
+package classes;
+
+import classes.abstractClasses.Stage;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Race {
+    private final ReentrantLock win  = new ReentrantLock();
     private ArrayList<Stage> stages;
     public ArrayList<Stage> getStages() { return stages; }
     public Race(Stage... stages) {
         this.stages = new ArrayList<>(Arrays.asList(stages));
     }
+    ReentrantLock getWin(){ return win; }
 }
