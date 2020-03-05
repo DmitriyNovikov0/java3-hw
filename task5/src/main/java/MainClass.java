64defc3d93bd5d753d0484ae193b448fbbb67eff
DmitriyNovikov0 <47492056+DmitriyNovikov0@users.noreply.github.com>
2020-03-05 22:54:24 +0600

    Update MainClass.java

--- task5/src/main/java/MainClass.java
+++ task5/src/main/java/MainClass.java
@@ -1,18 +1,43 @@
+import classes.Car;
+import classes.Race;
+import classes.Road;
+import classes.Tunnel;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
 public class MainClass {
-
     public static final int CARS_COUNT = 4;
-
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        CountDownLatch start = new CountDownLatch(CARS_COUNT);
+        CyclicBarrier barrierStart = new CyclicBarrier(CARS_COUNT);
+        CountDownLatch finish = new CountDownLatch(CARS_COUNT);
+        Semaphore semaphore = new Semaphore(CARS_COUNT / 2);
+        Race race = new Race(new Road(60), new Tunnel(semaphore), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), barrierStart, start, finish);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        try {
+            start.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            finish.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
+
+
+
+
+
+
+
+
